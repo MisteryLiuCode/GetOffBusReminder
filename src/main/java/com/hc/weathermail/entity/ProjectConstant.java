857d69175b79a11406dc83cc2cb4dce86d20585b
MisteryLiuCode <77911473+MisteryLiuCode@users.noreply.github.com>
2023-04-09 23:28:35 +0800

    初始化

---
+++ src/main/java/com/hc/weathermail/entity/ProjectConstant.java
@@ -0,0 +1,5 @@
+package com.hc.weathermail.entity;
+
+public class ProjectConstant {
+    public static String HEFENGWEATHERMAIL_CONFIG = "HeFengWeathermail.properties";
+}
---
+++ src/main/java/com/hc/weathermail/entity/walkingPlan/WalkingPlanVO.java
@@ -0,0 +1,58 @@
+/**
+ * Copyright 2023 bejson.com
+ */
+package com.hc.weathermail.entity.walkingPlan;
+
+import com.hc.weathermail.entity.addressEncode.Geocodes;
+
+import java.util.List;
+
+/**
+ * Auto-generated: 2023-04-04 11:26:13
+ *
+ * @author bejson.com (i@bejson.com)
+ * @website http://www.bejson.com/java2pojo/
+ */
+public class WalkingPlanVO {
+
+    private String status;
+    private String info;
+    private String infocode;
+    private String count;
+    private Route route;
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    public String getStatus() {
+        return status;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfocode(String infocode) {
+        this.infocode = infocode;
+    }
+    public String getInfocode() {
+        return infocode;
+    }
+
+    public void setCount(String count) {
+        this.count = count;
+    }
+    public String getCount() {
+        return count;
+    }
+
+    public Route getRoute() {
+        return route;
+    }
+
+    public void setRoute(Route route) {
+        this.route = route;
+    }
+}
